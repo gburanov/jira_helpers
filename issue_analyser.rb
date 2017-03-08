df49e4a90c3a6a0890ce52ea74ca52e53c014b6f
Georgy Buranov <gburanov@gmail.com>
2017-03-08 16:27:34 +0530

    some fixes

--- issue_analyser.rb
+++ issue_analyser.rb
@@ -1,12 +1,17 @@
 FT2USERS = ['alfredo.castaneda', 'francisco.sokol', 'georgy.buranov', 'post.vittorius', 'adrienmoreau']
 
 class IssueAnalyser
-  def initialize(issue)
+  attr_reader :issue
+  attr_reader :client
+
+  def initialize(client, issue)
     @issue = issue
+    @client = client
   end
-  
+
   def call
     key = issue.key
+    return if issue.summary.downcase.include?('plan') && issue.summary.downcase.include?('sprint')
     subtasks_total = []
     subtasks_noestimation = []
     subtasks = JIRA::Resource::Issue.jql(client, "parent = #{key}")
@@ -14,13 +19,19 @@ class IssueAnalyser
       assignee = subtask.assignee.name
       next unless FT2USERS.include?(assignee)
       subtasks_total << subtask
-      total_time = subtask.progress['total']
-      subtasks_noestimation << subtask if total_time.zero?
+      total_time = subtask.aggregatetimeoriginalestimate
+      subtasks_noestimation << subtask if total_time.nil? || total_time.zero?
     end
     if subtasks_total.count == 0 || subtasks_noestimation.count > 0
       puts "#{issue.key} - #{issue.summary} - for #{issue.assignee.name}"
       puts "Total number of subtasks #{subtasks_total.count}"
       puts "From them unestimated #{subtasks_noestimation.count}"
     end
+  end
+
+  def filter_issue?
+  end
+
+  def filter_subtask?
   end
 end
--- subtasks.rb
+++ subtasks.rb
@@ -1,6 +1,7 @@
 require 'jira-ruby'
 require 'byebug'
 require 'dotenv'
+require_relative 'issue_analyser'
 Dotenv.load
 
 options = {
@@ -15,13 +16,13 @@ startDate = Date.parse('22.02.2017')
 
 client = JIRA::Client.new(options)
 
-issue = client.Issue.find("AB-3944")
-
+issue = client.Issue.find("AB-5707")
+IssueAnalyser.new(client, issue).call
 
 filter = client.Filter.find('26002')
 issues = JIRA::Resource::Issue.jql(client, filter.jql, start_at: nil, max_results: 1000)
 
 issues.each do |issue|
   issue.fetch
-  IssueAnalyser.new(issue).call
+  IssueAnalyser.new(client, issue).call
 end
